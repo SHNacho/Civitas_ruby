2ef1daf673a39086399b8a97c55bbff6a5261cc1
SHNacho <nachosanchezh@hotmail.com>
2019-09-19 14:32:43 +0200

    first commit

---
+++ lib/main.rb
@@ -0,0 +1,28 @@
+
+module Civitas
+  module TipoCasilla
+    CALLE = :calle
+    SORPRESA = :sorpresa
+    JUEZ = :juez
+    IMPUESTO = :impuesto
+    DESCANSO = :descanso
+  end
+  
+  module TipoSorpresa
+    IRCARCEL = :ircarcel
+    IRCASILLAS = :ircasilla
+    PAGARCOBRAR = :pagarcobrar
+    PORCASAHOTEL = :pocashotel
+    PORJUGADOR = :porjugador
+    SALIRCARCEL = :salircarcel
+  end
+  
+  module Estados_juego
+		INICIO_TURNO = :inicio_turno  
+		DESPUES_CARCEL :despues_carcel
+    DESPUES_AVANZAR = :despues_avanzar
+    DESPUES_COMPRAR =:despues_comprar
+    DESPUES_GESTIONAR =:despues_gestionar
+  end
+  
+end
