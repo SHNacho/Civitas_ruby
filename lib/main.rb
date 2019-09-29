928bbb58d6e3859edf92f1e20ab9884f31e1793b
SHNacho <nachosanchezh@hotmail.com>
2019-09-29 11:19:33 +0200

    Civitas

--- lib/main.rb
+++ lib/main.rb
@@ -1,28 +1,46 @@
 
+require_relative 'tablero.rb'
+require_relative 'sorpresa.rb'
+require_relative 'mazo_sorpresas.rb'
+
 module Civitas
   module TipoCasilla
-    CALLE = :calle
+    CALLE    = :calle
     SORPRESA = :sorpresa
-    JUEZ = :juez
+    JUEZ     = :juez
     IMPUESTO = :impuesto
     DESCANSO = :descanso
   end
   
   module TipoSorpresa
-    IRCARCEL = :ircarcel
-    IRCASILLAS = :ircasilla
-    PAGARCOBRAR = :pagarcobrar
+    IRCARCEL     = :ircarcel
+    IRCASILLA    = :ircasilla
+    PAGARCOBRAR  = :pagarcobrar
     PORCASAHOTEL = :pocashotel
-    PORJUGADOR = :porjugador
-    SALIRCARCEL = :salircarcel
+    PORJUGADOR   = :porjugador
+    SALIRCARCEL  = :salircarcel
   end
   
   module Estados_juego
-		INICIO_TURNO = :inicio_turno  
-		DESPUES_CARCEL :despues_carcel
-    DESPUES_AVANZAR = :despues_avanzar
-    DESPUES_COMPRAR =:despues_comprar
-    DESPUES_GESTIONAR =:despues_gestionar
+		INICIO_TURNO      = :inicio_turno
+		DESPUES_CARCEL    = :despues_carcel
+		DESPUES_AVANZAR   = :despues_avanzar
+		DESPUES_COMPRAR   = :despues_comprar
+		DESPUES_GESTIONAR = :despues_gestionar
   end
+  
+  mazo       = Civitas::MazoSorpresas.new
+  sorpresa_1 = Civitas::Sorpresa.new("a")
+  sorpresa_2 = Civitas::Sorpresa.new("b")
+  sorpresa_3 = Civitas::Sorpresa.new("c")
+  sorpresa_4 = Civitas::Sorpresa.new("d")
+  sorpresa_5 = Civitas::Sorpresa.new("e")
+  mazo.al_mazo(sorpresa_1)
+  mazo.al_mazo(sorpresa_2)
+  mazo.al_mazo(sorpresa_3)
+  mazo.al_mazo(sorpresa_4)
+  mazo.al_mazo(sorpresa_5)
+  sorpresa = mazo.siguiente
+  puts sorpresa.nombre
   
 end
---
+++ lib/tablero.rb
@@ -0,0 +1,105 @@
+# encoding:utf-8
+
+module Civitas
+  class Casilla #Clase temporal
+    
+    attr_reader :nombre
+    
+    def initialize (_nombre)
+      @nombre = _nombre
+    end
+  end
+  
+  class Tablero
+    
+    attr_reader :num_casilla_carcel
+    
+    def initialize (_num_casilla_carcel)
+      if _num_casilla_carcel >= 1
+        @num_casilla_carcel = _num_casilla_carcel
+      else
+        @num_casilla_carcel = 1
+      end
+      @casillas = []
+      @casillas[0] = Casilla.new("Salida")
+      @por_salida = 0
+      @tiene_juez = false
+    end
+    
+    private
+    def correcto()
+      return (@casillas.size > num_casilla_carcel) && @tiene_juez
+    end
+    
+    def correcto(num_casilla)
+      es_correcto = false
+      if self.correcto && (num_casilla < @casillas.size)
+        es_correcto = true
+      end
+      
+      return es_correcto
+    end
+    
+    public
+    
+    def por_salida
+      valor = @por_salida
+      if @por_salida > 0
+        @por_salida -= 1
+      end
+      
+      return valor
+    end
+    
+    def añade_casilla(casilla) 
+      if @casillas.size == @num_casilla_carcel
+        @casillas.push(Casilla.new("Cárcel"))
+      end
+      @casillas.push(casilla)
+      if @casillas.size == @num_casilla_carcel
+        @casillas.push(Casilla.new("Cárcel"))
+      end
+    end
+    
+    def añade_juez
+      if !@tiene_juez
+        self.añade_casilla(Casilla.new("Juez"))
+        @tiene_juez = true
+      end
+    end
+    
+    def get_casilla(num_casilla)
+      casilla = nil
+      if num_casilla < @casillas.size
+        casilla = @casillas[num_casilla]
+      end
+      return casilla
+    end
+    
+    def nueva_posicion(actual, tirada)
+      posicion = actual
+      modulo = actual % @casillas.size
+      if (modulo) < tirada
+        posicion = tirada - modulo
+        por_salida += 1
+      else
+        posicion += tirada
+      end
+      
+      return posicion
+    end
+    
+    def calcular_tirada(origen, destino)
+      tirada = destino - origen
+      if (tirada < 0)
+        tirada += @casillas.size
+      end
+      
+      return tirada
+    end
+   
+    
+  end
+  
+end
+
