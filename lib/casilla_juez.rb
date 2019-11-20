55ded1e8e9e110e3e9fd99a714213f506a95572e
juliocamposrd <juliocamposrodriguez@192.168.1.23>
2019-11-20 16:27:52 +0100

    Merge origin/master
    Conflicts:
    	src/civitas/Casilla.java
    	src/civitas/CivitasJuego.java
    	src/civitas/Jugador.java

---
+++ lib/casilla_juez.rb
@@ -0,0 +1,32 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Casilla_juez < Casilla
+  
+  @@carcel = 0
+  
+  def initialize(nombre, num_casilla_carcel)
+    super (nombre)
+    @@carcel = num_casilla_carcel
+  end
+  
+  def recibe_jugador (i_actual, todos)
+    if jugador_correcto(i_actual, todos)
+        informe(i_actual, todos)
+        todos[i_actual].encarcelar(@@carcel)
+    end
+  end
+  
+  def to_s
+    str = "-------------------------------------------\n" +
+            "CASILLA: \n" + "Nombre:            " + @nombre + "\n" +
+                            "Tipo:              " + "Juez" + "\n"
+                            
+    str+= "Casilla carcel:    " + @@carcel.to_s + "\n"
+    
+    str += "-------------------------------------------\n"
+      
+    return str
+  end
+end
